3b4ae63edfa40d16e3592d3adee5c40b9c930239
ignaciomarquezalbes <ignacio.marquez.albes@gmail.com>
2025-06-28 11:03:28 +0200

    Create problem_data.m

---
+++ euler-implicit/problem_data.m
@@ -0,0 +1,13 @@
+function[f,a,eta,h,npas,sol,isol]=problem_data
+
+h= 1.000e-03;    %step
+npas=99;         %number of steps
+
+f='function';
+sol='solution';
+isol=1;
+
+a=1.000e-03;     %x initial condition
+eta=1.053e-03;   %y initial condition
+
+end
